4d67ea1a649ddfedc4150c9eea3ed488fa3bf2c3
Rayato159 <rayato159@gmail.com>
2023-03-18 02:23:47 +0700

    -refactor :package: refactor module struct

--- modules/monitor/handlers/monitor_handler.go
+++ modules/monitor/handlers/monitor_handler.go
@@ -12,19 +12,19 @@ type IMonitorHandler interface {
 }
 
 type monitorHandler struct {
-	Cfg config.IConfig
+	cfg config.IConfig
 }
 
 func MonitorHandler(cfg config.IConfig) IMonitorHandler {
 	return &monitorHandler{
-		Cfg: cfg,
+		cfg: cfg,
 	}
 }
 
 func (h *monitorHandler) HealthCheck(c *fiber.Ctx) error {
 	res := &monitor.Monitor{
-		Name:    h.Cfg.App().Name(),
-		Version: h.Cfg.App().Version(),
+		Name:    h.cfg.App().Name(),
+		Version: h.cfg.App().Version(),
 	}
 	return entities.NewResponse(c).Success(fiber.StatusOK, res).Res()
 }
