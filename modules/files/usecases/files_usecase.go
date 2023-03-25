4989881c0a5bd6ea9458eb2cc0f61dbf80c0d189
Rayato159 <rayato159@gmail.com>
2023-03-25 23:12:40 +0700

    -refactor :package: change upload file to pool workers

--- modules/files/usecases/files_usecase.go
+++ modules/files/usecases/files_usecase.go
@@ -7,7 +7,6 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
-	"sync"
 	"time"
 
 	"cloud.google.com/go/storage"
@@ -55,6 +54,55 @@ func (f *fileRes) public() error {
 	return nil
 }
 
+func (u *filesUsecase) uploadWorkers(ctx context.Context, client *storage.Client, jobs <-chan *filespkg.FileReq, results chan<- *filespkg.FileRes, errChan chan<- error) {
+	for job := range jobs {
+		container, err := job.File.Open()
+		if err != nil {
+			errChan <- err
+			return
+		}
+		b, err := ioutil.ReadAll(container)
+		if err != nil {
+			errChan <- err
+			return
+		}
+
+		buf := bytes.NewBuffer(b)
+
+		// Upload an object with storage.Writer.
+		wc := client.Bucket(u.cfg.App().GCPBucket()).Object(job.Destination).NewWriter(ctx)
+
+		if _, err = io.Copy(wc, buf); err != nil {
+			errChan <- fmt.Errorf("io.Copy: %v", err)
+			return
+		}
+		// Data can continue to be added to the file until the writer is closed.
+		if err := wc.Close(); err != nil {
+			errChan <- fmt.Errorf("Writer.Close: %v", err)
+			return
+		}
+		log.Printf("%v uploaded to %v.\n", job.FileName, job.Destination)
+
+		newFile := &fileRes{
+			file: &filespkg.FileRes{
+				Url:      fmt.Sprintf("https://storage.googleapis.com/%s/%s", u.cfg.App().GCPBucket(), job.Destination),
+				Filename: job.FileName,
+			},
+			destination: job.Destination,
+			bucket:      u.cfg.App().GCPBucket(),
+		}
+
+		// Make obj to public access
+		if err := newFile.public(); err != nil {
+			errChan <- err
+			return
+		}
+
+		// Assign result
+		results <- newFile.file
+	}
+}
+
 func (u *filesUsecase) UploadToGCP(req []*filespkg.FileReq) ([]*filespkg.FileRes, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*60)
 	defer cancel()
@@ -65,69 +113,26 @@ func (u *filesUsecase) UploadToGCP(req []*filespkg.FileReq) ([]*filespkg.FileRes
 	}
 	defer client.Close()
 
+	// Chan declare
+	jobsCh := make(chan *filespkg.FileReq, len(req))
+	resultsCh := make(chan *filespkg.FileRes, len(req))
+	errsCh := make(chan error, len(req))
+
+	// Return value
 	res := make([]*filespkg.FileRes, 0)
 
-	var wg sync.WaitGroup
-	wg.Add(len(req))
-	errChan := make(chan error)
-	for i := range req {
-		go func(i int) {
-			defer wg.Done()
-
-			container, err := req[i].File.Open()
-			if err != nil {
-				errChan <- err
-				return
-			}
-			b, err := ioutil.ReadAll(container)
-			if err != nil {
-				errChan <- err
-				return
-			}
-
-			buf := bytes.NewBuffer(b)
-
-			// Upload an object with storage.Writer.
-			wc := client.Bucket(u.cfg.App().GCPBucket()).Object(req[i].Destination).NewWriter(ctx)
-
-			if _, err = io.Copy(wc, buf); err != nil {
-				errChan <- fmt.Errorf("io.Copy: %v", err)
-				close(errChan)
-				return
-			}
-			// Data can continue to be added to the file until the writer is closed.
-			if err := wc.Close(); err != nil {
-				errChan <- fmt.Errorf("Writer.Close: %v", err)
-				close(errChan)
-				return
-			}
-			log.Printf("%v uploaded to %v.\n", req[i].FileName, req[i].Destination)
-
-			newFile := &fileRes{
-				file: &filespkg.FileRes{
-					Url:      fmt.Sprintf("https://storage.googleapis.com/%s/%s", u.cfg.App().GCPBucket(), req[i].Destination),
-					Filename: req[i].FileName,
-				},
-				destination: req[i].Destination,
-				bucket:      u.cfg.App().GCPBucket(),
-			}
-
-			// Make obj to public access
-			if err := newFile.public(); err != nil {
-				errChan <- err
-				close(errChan)
-				return
-			}
-
-			res = append(res, newFile.file)
-		}(i)
+	for _, f := range req {
+		jobsCh <- f
 	}
-	wg.Wait()
-	close(errChan)
 
-	errUpload := <-errChan
-	if err != nil {
-		return nil, errUpload
+	numWorkers := 5
+	for i := 0; i < numWorkers; i++ {
+		go u.uploadWorkers(ctx, client, jobsCh, resultsCh, errsCh)
+	}
+
+	for a := 0; a < len(req); a++ {
+		result := <-resultsCh
+		res = append(res, result)
 	}
 
 	return res, nil
